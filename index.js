9700ed617931324bb49e63e5f64e56b32908633d
Sean Sutton Jr <sutton_mjbc@icloud.com>
2018-08-01 15:15:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign()
+  var newObj = Object.assign({}, key)
 }
